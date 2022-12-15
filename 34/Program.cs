ce51917f115fa242775d7ddebeb35c4fc97ae2c9
Assemchik1 <118967701+Assemchik1@users.noreply.github.com>
2022-12-16 01:10:45 +0500

    34

---
+++ 34/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 34: Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел 
+// в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+int[] CreateArray(int arraySize)
+{
+    int[] array = new int [arraySize];
+    for(int i = 0; i < arraySize; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+Console.WriteLine(CreateArray(size));
